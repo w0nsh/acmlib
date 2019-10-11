e038ed507ee2052d49b936cea6a47928875c5284
MarcinKnapik37 <45509637+MarcinKnapik37@users.noreply.github.com>
2019-10-11 10:22:37 +0200

    Add files via upload

---
+++ templates/marcin.cpp
@@ -0,0 +1,20 @@
+//Marcin Knapik
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define FOR(i, n) for(int i=0; i<n; i++)
+#define pb push_back
+#define vi vector<int>
+#define ii pair<int, int>
+#define f first 
+#define s second
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    	
+}
